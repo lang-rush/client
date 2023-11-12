8b5c31b794570215e9a2ea57c8d6a90451cc2d4d
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-11-12 11:23:36 +0200

    feat: delete Icon

--- src/Features/Folders/Components/Folder/Folder.tsx
+++ src/Features/Folders/Components/Folder/Folder.tsx
@@ -1,9 +1,8 @@
 import { useAppSelector } from "src/store/store";
 import folderImage from "@assets/images/folder.svg";
+import deleteIcon from "@assets/images/delete.svg";
 import s from "./Folder.module.scss";
 import { FC } from "react";
-import { ReactSVG } from "react-svg";
-
 interface IFolderProps {
   id: string;
   name: string;
@@ -14,8 +13,11 @@ const Folder: FC<IFolderProps> = ({ name }) => {
 
   return (
     <div className={isDark ? `${s.container} ${s.dark}` : s.container}>
-      <ReactSVG src={folderImage} className={s.image} />
+      <img src={folderImage} className={s.image} />
       <p className={s.name}>{name}</p>
+      <div className={s.delete}>
+        <img src={deleteIcon} className={s.image} />
+      </div>
     </div>
   );
 };
